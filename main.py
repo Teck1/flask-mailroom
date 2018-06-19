5a92431072602ae41c3b60da2d0ab673a57eee71
Teck1 <Teck1@users.noreply.github.com>
2018-06-19 16:02:00 -0700

    Initial commit

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 def home():
@@ -16,6 +18,24 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['name']).get()
+        except Donor.DoesNotExist:
+            donor = Donor(name=request.form['name'])
+            donor.save()
+
+        amount = request.form['amount']
+        if amount == '':
+            amount = '0'
+        new_donation = Donation(value=amount, donor=donor)
+        new_donation.save()
+
+        return redirect(url_for('home'))
+
+    return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
